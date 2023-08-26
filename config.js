f8add893578a623454c4e185fc8fb80938932a1e
Leduong1 <99656475+Leduong1@users.noreply.github.com>
2023-08-26 22:49:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100039453280084';
+const eventName = 'GỬI TẶNG BÉ😘😘🥰🥰🥰';
+const titleCard = 'Tặng TRÚC THANH';
+const contentCard = 'Chúc bạn 26/8 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'FB_IMG_1666192071901.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
